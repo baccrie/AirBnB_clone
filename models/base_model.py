c1ac66cbf88bcf60818e30697ebe42cc098c7e3d
baccrie <bakarerilwan1@gmail.com>
2022-09-01 23:39:35 +0100

    Task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 
 """"
@@ -8,9 +9,10 @@ A module that contains the base class for all other classes
 """
 
 class BaseModel:
-    """""
+    """
     The base class for all other classes
     """
+    
     def __init__(self, *args, **kwargs):
         if (kwargs) and (kwargs != ""):
             for key in kwargs:
@@ -24,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         
     def __str__(self):
         """
@@ -36,6 +39,7 @@ class BaseModel:
         A method that updates the current date and time of an instance when invoked
         """
         self.updated_at = datetime.now()
+        models.storage.save()
         
     def to_dict(self):
         """"
---
+++ models/engine/file_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""""
+A file storage engine module
+"""
+
+import json
+import os
+from models.base_model import BaseModel
+
+class FileStorage:
+    """
+    A module class that serialises and deserialises
+    an instance of classes generated from BaseModel
+    """
+    __file_path = "file.json"
+    __objects = {}
+    
+    def allClass(self):
+        """
+        This method is used to import classes
+        from models.base_model import BaseModel
+        """
+        
+    def all(self):
+        """
+        Returns a dict containing all instance created
+        """
+        return (FileStorage.__objects)
+    
+    def new(self, obj):
+        """
+        serialises a new instance to the private class attr __objects
+        """
+        key = type(self).__name__,obj.id
+        FileStorage.__objects[key] = obj
+        
+    def save(self):
+        """
+        A method that serialises the object attr to a file
+        """
+        new = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(new, f)
+            
+    def reload(self):
+        """
+        A method that deserializes the JSON file to __object
+        """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'w') as f:
+                dict = json.load(f)
+                new_dict = {key: self.classes()[value["__class__"]](**value)\
+                for key, value in dict}
+                
+        else:
+            pass

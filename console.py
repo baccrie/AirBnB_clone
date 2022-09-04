a74669881c0e11215248b8b63f5ab533c579c42e
baccrie <bakarerilwan1@gmail.com>
2022-09-04 10:08:41 +0100

    Task 6 Fixed

--- console.py
+++ console.py
@@ -13,9 +13,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     
     def do_quit(self, line):
+        """
+        Exits the interactive shell session
+        """
         return (True)
     
     def do_EOF(self, line):
+        """
+        Exits the interactive shell session
+        """
+        print()
         return (True)   
     
 if __name__ == "__main__":

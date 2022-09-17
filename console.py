54b83e92148ee51cadd3519aacff6a95e18a067c
baccrie <bakarerilwan1@gmail.com>
2022-09-17 23:55:59 +0100

    Task

--- console.py
+++ console.py
@@ -40,15 +40,15 @@ class HBNBCommand(cmd.Cmd):
         attr = attr.split()
         
         if not attr:
-            print("** class name missing")
+            print("** class name missing **")
         elif attr[0] not in self.classes:
-            print("** class dosen't exist**")
+            print("** class dosen't exist **")
         else:
             new = BaseModel()
             new.save()
             print(new.id)
             
-    def do_show(self, attr):
+    def do_show (self, attr):
         """
         Prints the dictionary repr of an object with Class name and id
         """
@@ -59,18 +59,83 @@ class HBNBCommand(cmd.Cmd):
                 print("** class dosent exist **")
         elif (len(attr) == 1):
             print("** instance id missing **")
-        else:
-            storage.reload()
-            obj = storage.all()
+        elif (attr and attr[1]):
             attr_2 = f"{attr[0]}.{attr[1]}"
-            for key, value in obj.items():
-                if key == attr_2:
-                    print(value)
+            if attr_2 not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                storage.reload()
+                obj = storage.all()
+                attr_2 = f"{attr[0]}.{attr[1]}"
+                for key, value in obj.items():
+                    if key == attr_2:
+                        print(value)
+    
+    def do_destroy (self, attr):
+        """
+        Deletes an instance from the file storage engine
+        """
+        attr = attr.split()
+        if not attr:
+            print("** class name is missing **")
+        elif attr and attr[0] not in self.classes:
+            print("** class dosen't exist **")
+        elif len(attr) == 1:
+            print("** instance id is missing **")
+        elif (attr and attr[1]):
+            attr_2 = f"{attr[0]}.{attr[1]}"
+            if attr_2 not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                key = f"{attr[0]}.{attr[1]}"
+                storage.all().pop(key)
+                storage.save()
             
+    def do_all (self, attr):
+        """
+         Prints all string representation of all instances based or not on the class name
+         """
+        attr = attr.split()
+        if (not attr):
+            obj = [f"{values}" for key, values in storage.all().items()]
+        
+        else:
+            obj = [f"{values}" for key, values in storage.all().items() if attr[0] == type(values).__name__]
             
+        print(obj)
+        
+    def do_update (self, attr):
+        """"
+        tmp
+        """
+        attr = attr.split()
+        if not attr:
+            print("** class name is missing **")
+        elif attr and attr[0] not in self.classes:
+            print("** class dosen't exist **")
+        elif len(attr) == 1:
+            print("** instance id is missing **")
+        elif (attr and attr[1]):
+            attr_2 = f"{attr[0]}.{attr[1]}"
+            if attr_2 not in storage.all().keys():
+                print("** no instance found **")
+        elif (len(attr) == 2):
+            print("** attribute name missing **")
+        elif (len(attr) == 3):
+            print("** value missing **")
+        else:
+             key = f"{attr[0]}.{attr[1]}"
+             obj = storage.all()
+             for keys, values in obj.items():
+                if keys == key:
+                     values[attr[2]] = attr[3]
+                storage.save()
+                 
             
-            
-            
+                
+
+        
+        
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

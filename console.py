31270649a5cf9e4b2643bb2c5a89bb6c2f839827
baccrie <bakarerilwan1@gmail.com>
2022-09-18 11:13:50 +0100

    Task done

--- console.py
+++ console.py
@@ -14,31 +14,31 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
     classes = ["BaseModel"]
-    
+
     def do_quit(self, line):
         """
         Exits the interactive shell session
         """
         return (True)
-    
+
     def do_EOF(self, line):
         """
         Exits the interactive shell session
         """
-        return (True)   
-    
+        return (True)
+
     def emptyline(self):
-        """"
+        """
         a method that ensures nothing is printed when an empty line is executed
         """
-        pass    
-    
+        pass
+
     def do_create(self, attr):
         """
-        This method cretaes a new instance of the base model or base model descandants
+        This method cretaes a new instance of the base
+        model or base model descandants
         """
         attr = attr.split()
-        
         if not attr:
             print("** class name missing **")
         elif attr[0] not in self.classes:
@@ -47,8 +47,8 @@ class HBNBCommand(cmd.Cmd):
             new = BaseModel()
             new.save()
             print(new.id)
-            
-    def do_show (self, attr):
+
+    def do_show(self, attr):
         """
         Prints the dictionary repr of an object with Class name and id
         """
@@ -56,7 +56,7 @@ class HBNBCommand(cmd.Cmd):
         if (not attr):
             print("** class name missing **")
         elif (attr and attr[0] not in self.classes):
-                print("** class dosent exist **")
+            print("** class dosent exist **")
         elif (len(attr) == 1):
             print("** instance id missing **")
         elif (attr and attr[1]):
@@ -70,8 +70,8 @@ class HBNBCommand(cmd.Cmd):
                 for key, value in obj.items():
                     if key == attr_2:
                         print(value)
-    
-    def do_destroy (self, attr):
+
+    def do_destroy(self, attr):
         """
         Deletes an instance from the file storage engine
         """
@@ -90,22 +90,22 @@ class HBNBCommand(cmd.Cmd):
                 key = f"{attr[0]}.{attr[1]}"
                 storage.all().pop(key)
                 storage.save()
-            
-    def do_all (self, attr):
+
+    def do_all(self, attr):
         """
-         Prints all string representation of all instances based or not on the class name
+         Prints all string representation of all
+         instances based or not on the class name
          """
         attr = attr.split()
         if (not attr):
             obj = [f"{values}" for key, values in storage.all().items()]
-        
         else:
-            obj = [f"{values}" for key, values in storage.all().items() if attr[0] == type(values).__name__]
-            
+            obj = [f"{values}" for key, values in storage.all().items()
+                    \if attr[0] == type(values).__name__]
         print(obj)
-        
-    def do_update (self, attr):
-        """"
+
+    def do_update(self, attr):
+        """
         tmp
         """
         attr = attr.split()
@@ -124,18 +124,13 @@ class HBNBCommand(cmd.Cmd):
         elif (len(attr) == 3):
             print("** value missing **")
         else:
-             key = f"{attr[0]}.{attr[1]}"
-             obj = storage.all()
-             for keys, values in obj.items():
+            key = f"{attr[0]}.{attr[1]}"
+            obj = storage.all()
+            for keys, values in obj.items():
                 if keys == key:
-                     values[attr[2]] = attr[3]
-                storage.save()
-                 
-            
-                
+                    values[attr[2]] = attr[3]
+            storage.save()
 
-        
-        
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

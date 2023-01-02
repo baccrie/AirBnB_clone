667097bb4748b84c615bcdbc213da6139b99f2bc
baccrie <bakarerilwan1@gmail.com>
2023-01-02 20:12:08 +0100

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,24 @@ from datetime import datetime
 class BaseModel:
     """A Base class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialization of object"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs and kwargs is not None:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    pass
+                elif (key == 'created_at'):
+                    self.created_at = datetime.\
+                            strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                elif (key == 'updated_at'):
+                    self.updated_at = datetime.\
+                            strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints the str repr of an object"""
@@ -24,7 +37,7 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute updated_at
-with the current datetime"""
+    with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
---
+++ models/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+class Student:
+  marks = 88
+
+person = Student()
+
+# set value of name to Adam
+setattr(person, 'name', 'Adam')
+print(person.name)
+
+# set value of marks to 78
+setattr(person, 'marks', 78)
+print(person.marks)
+
+# Output: Adam
+#         78

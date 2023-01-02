fe48ec6b86ee4e1e7759a21884233221464ad929
baccrie <bakarerilwan1@gmail.com>
2023-01-02 17:32:38 +0100

    Fix Readme

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    name = ''
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    state_id = ''
-    name = ''

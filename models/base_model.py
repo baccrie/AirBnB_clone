85750d0314191e13e52fe8cc0a1bac39a0a41022
baccrie <bakarerilwan1@gmail.com>
2023-01-02 18:59:15 +0100

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""A class named (BaseModel) that defines all common attributes/methods for other classes:"""
+"""A class named (BaseModel) that defines all common
+attributes/methods for other classes:"""
+
 
 import uuid
 from datetime import datetime
+
 
 class BaseModel:
     """A Base class"""
@@ -15,15 +18,18 @@ class BaseModel:
 
     def __str__(self):
         """Prints the str repr of an object"""
-        ret_value = f"{[type(self).__name__]} {(self.id)} {self.__dict__}"
+        ret_value = "[{}] ({}) {}"\
+            .format(type(self).__name__, self.id, self.__dict__)
         return (ret_value)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute updated_at
+with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance:"""
+        """returns a dictionary containing all keys/values of
+__dict__ of the instance:"""
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = type(self).__name__
         dict_copy['created_at'] = dict_copy['created_at'].isoformat()

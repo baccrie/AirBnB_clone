724cc717ff9eb464d2d67d69d87ae616eba882da
baccrie <bakarerilwan1@gmail.com>
2023-01-03 07:51:03 +0100

    Task

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,7 +25,7 @@ class FileStorageTests(unittest.TestCase):
         all_objs = storage.all()
 
         key = bm_dict['__class__'] + "." + bm_dict['id']
-        #self.assertEqual(key in all_objs, True)
+        self.assertEqual(key in all_objs, True)
 
     def testStoreBaseModel2(self):
         """ Test save, reload and update functions """
@@ -36,7 +36,7 @@ class FileStorageTests(unittest.TestCase):
 
         key = bm_dict['__class__'] + "." + bm_dict['id']
 
-        self.assertEqual(key in all_objs, False)
+        self.assertEqual(key in all_objs, True)
         self.assertEqual(bm_dict['my_name'], "First name")
 
         create1 = bm_dict['created_at']

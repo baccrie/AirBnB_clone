b89905cae96fd898424adc98d0ba75009198b054
baccrie <bakarerilwan1@gmail.com>
2023-01-03 23:02:18 +0100

    Task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@
 import json
 import os
 from models.base_model import BaseModel
-
+from models.user import User
 
 class FileStorage:
     """A class that handles serialization and de-serialization"""

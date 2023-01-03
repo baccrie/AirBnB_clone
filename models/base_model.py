c1b1df2258ff481c28196b6e5f12f7240fa02310
baccrie <bakarerilwan1@gmail.com>
2023-01-03 08:55:38 +0100

    Test Base

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,52 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
-from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import String
+"""A class named (BaseModel) that defines all common
+attributes/methods for other classes:"""
 
-Base = declarative_base()
+
+import uuid
+from datetime import datetime
+import models
 
 
 class BaseModel:
-    """Defines the BaseModel class.
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
-    """
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """A Base class"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
-        if kwargs:
+        """initialization of object"""
+        if kwargs and kwargs is not None:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                if key == '__class__':
+                    pass
+                elif (key == 'created_at'):
+                    self.created_at = datetime.\
+                            strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                elif (key == 'updated_at'):
+                    self.updated_at = datetime.\
+                            strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                else:
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Prints the str repr of an object"""
+        ret_value = "[{}] ({}) {}"\
+            .format(type(self).__name__, self.id, self.__dict__)
+        return (ret_value)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance.
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict.pop("_sa_instance_state", None)
-        return my_dict
-
-    def delete(self):
-        """Delete the current instance from storage."""
-        models.storage.delete(self)
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+        """returns a dictionary containing all keys/values of
+__dict__ of the instance:"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = type(self).__name__
+        dict_copy['created_at'] = dict_copy['created_at'].isoformat()
+        dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
+        return (dict_copy)

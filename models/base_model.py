fe2672e8d358033e0bb43c99177d1164af54feb7
baccrie <bakarerilwan1@gmail.com>
2023-01-10 10:07:25 +0100

    Task 0

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
 
     def __init__(self):
         """an instance method"""
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 

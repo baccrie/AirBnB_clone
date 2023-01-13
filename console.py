8bf0184dc8dc33d309efb5933ae9c213a371b76a
baccrie <bakarerilwan1@gmail.com>
2023-01-13 08:40:41 +0100

    Task 6

--- console.py
+++ console.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+"""The great console module"""
 
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Entry point for the command interpreter"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Exits the console safely"""
+        return True
+
+    def do_EOF(self, line):
+        """Quits the console"""
+        return True
+
+    def do_readline(self):
+        """a method that ensures that
+        an empty line + ENTER
+        shouldn’t execute anything"""
+        print()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

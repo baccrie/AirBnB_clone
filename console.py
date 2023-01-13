9e27d42a10498ddcc1948fe68a0500cbf624e08d
baccrie <bakarerilwan1@gmail.com>
2023-01-13 19:34:15 +0100

    Task

--- console.py
+++ console.py
@@ -3,9 +3,14 @@
 
 
 import cmd
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,7 +18,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     classes = {
             'BaseModel': BaseModel,
-            'User': User
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
             }
 
     def do_quit(self, line):

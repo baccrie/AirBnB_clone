692bc7ea1d983fc2fe2a3d872294e5963f2b4ead
baccrie <bakarerilwan1@gmail.com>
2023-01-13 18:50:45 +0100

    Task 8

--- models/user.py
+++ models/user.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
-"""A module that defines user"""
-
-
+"""User module"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """A class User that inherits from BaseModel:"""
+    """class User
+    Attributes:
+        email (str): User email.
+        password (str): User password.
+        first_name (str): User first name.
+        last_name (str): User last name.
+    """
     email = ""
     password = ""
     first_name = ""
